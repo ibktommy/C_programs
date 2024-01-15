693989d1aa5e8f7de7a02bd7352b30df0a13239b
ibktommy <atomquartz@gmail.com>
2024-01-15 07:33:53 +0100

    Using while and for loop to repeat an action based on a specified condition

--- meow.c
+++ meow.c
@@ -2,11 +2,17 @@
 
 int main(void)
 {
-  int counter = 0; 
+  //USING 'WHILE' LOOP
+  // int counter = 0; 
 
   // while loop to repeat statement till a condition is met
-  while (counter < 3) {
+  // while (counter < 3) {
+  //   printf("Meow\n");
+  //   counter++;
+  // }
+
+  // USING 'FOR' LOOP
+  for (int counter = 0; counter < 5; counter++) {
     printf("Meow\n");
-    counter++;
   }
 }
