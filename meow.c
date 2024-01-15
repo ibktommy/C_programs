4ed1a56e5405dd629d7dbff918983abbac152ae2
ibktommy <atomquartz@gmail.com>
2024-01-15 07:50:49 +0100

    Run a basic infinity loop

--- meow.c
+++ meow.c
@@ -3,16 +3,29 @@
 int main(void)
 {
   //USING 'WHILE' LOOP
-  // int counter = 0; 
+  /*
+    int counter = 0; 
 
-  // while loop to repeat statement till a condition is met
-  // while (counter < 3) {
-  //   printf("Meow\n");
-  //   counter++;
-  // }
+    while (counter < 3) 
+    {
+      printf("Meow\n");
+      counter++;
+    }
+  */
 
   // USING 'FOR' LOOP
-  for (int counter = 0; counter < 5; counter++) {
-    printf("Meow\n");
-  }
+  /*
+    for (int counter = 0; counter < 3; counter++)
+    {
+      printf("meow!\n");
+    }
+  */
+
+  // SIMPLE INFINITY LOOP
+  /*
+    while (1)
+    {
+      printf("meow!");
+    }
+  */
 }
