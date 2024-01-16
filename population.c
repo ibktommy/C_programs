7ee7a2dab58aac0b752e4f4c6f499e5c20a2d0d0
ibktommy <atomquartz@gmail.com>
2024-01-16 12:32:40 +0100

    Change data types of population sizes to int type and add some other variables

--- population.c
+++ population.c
@@ -1,23 +1,28 @@
 // Program to determine how long(years) it takes a population to reach a given population size in regards to newly-born and newly-dead figures.
 
 #include <stdio.h>
+#include <math.h>
 int main(void)
 {
   // Declare variables
-  int years;
+  int years = 0;
+  float birth_population = 0, death_population = 0, added_population = 0, growth_rate;
 
   // Prompt user for valid input, keep loop till inputs are valid
-  float start_population_size, end_population_size;
+  int start_population_size;
   do
   {
     printf("Enter initial population size: ");
-    scanf("%f", &start_population_size);
-
-    printf("Enter final population size: ");
-    scanf("%f", &end_population_size);
+    scanf("%i", &start_population_size);
   }
-  while (start_population_size < 9 || end_population_size < start_population_size);
+  while (start_population_size < 9);
 
-  // 
+  int end_population_size;
+  do 
+  {
+    printf("Enter final population size: ");
+    scanf("%i", &end_population_size);
+  }
+  while (end_population_size < start_population_size);
 }
 
